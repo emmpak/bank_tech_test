3889ef50d51272e4521fb8d7f2c5cc8335da7c4d
emmpak <eiliev05@yahoo.com>
2017-05-15 10:07:43 +0100

    Add statement test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,8 +2,13 @@ require 'account'
 
 describe Account do
   subject(:account) { described_class.new }
+  let(:statement) { double transactions: [] }
 
   it 'has an initial balance of 0' do
     expect(account.balance).to equal(0)
+  end
+
+  it 'initializes with an empty transaction history' do
+    expect(account.statement).to statement
   end
 end

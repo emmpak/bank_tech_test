67a73498743d869e59b5788543de1cf6809c76e6
emmpak <eiliev05@yahoo.com>
2017-05-15 10:12:36 +0100

    Add transaction history

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,13 +2,13 @@ require 'account'
 
 describe Account do
   subject(:account) { described_class.new }
-  let(:statement) { double transactions: [] }
+  # let(:statement) { double transactions: [] }
 
   it 'has an initial balance of 0' do
     expect(account.balance).to equal(0)
   end
 
   it 'initializes with an empty transaction history' do
-    expect(account.statement).to statement
+    expect(account.transaction_history).to be_empty
   end
 end
